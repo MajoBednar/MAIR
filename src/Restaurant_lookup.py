b36d5e632d1e9030182b2b775bcd5d2ce93ecc0f
JopParo <jopparo@gmail.com>
2025-09-16 20:52:53 +0200

    Create Restaurant_lookup.py

---
+++ src/Restaurant_lookup.py
@@ -0,0 +1,50 @@
+import pandas as pd 
+
+
+"""
+Function that takes as input pricerange area and food and recomends a restaurant.
+The lookup function prioritices the restaurant which fits on the most criterea.
+If there are multiple restaurants with the same number of corresponding criterea the importance wil be as follows:
+1. pricerange 
+2. food 
+3. area
+If there are restaurants with the same ammount of fittness a list is returned
+"""
+def restaurant_lookup(df, pricerange, area, food):
+    # Loop through all restaurants and safe a list with scores: 
+    # If restaurant has good price range add 1.1 
+    # If restaurant has good area add 1
+    # If restaurant has good food add 1.05 
+    # Return restaurant(s) with hihgest score
+
+    scores = []
+    for index, row in df.iterrows():
+        score = 0
+
+        if row['pricerange'] == pricerange:
+            score += 1.1
+        if row['area'] == area:
+            score += 1
+        if row['food'] == food:
+            score += 1.05
+
+        scores.append(score)
+
+    # Add scores to the dataframe
+    df['score'] = scores
+
+    # Get the maximum score
+    max_score = df['score'].max()
+  
+    # Define list with restaurants with max score
+    suitable_restaurants = df.loc[df['score'] == max_score, 'restaurantname'].tolist()
+
+    return suitable_restaurants
+
+
+def main():
+    restaurant_info = pd.read_csv('restaurant_info.csv')
+    print(restaurant_info)
+    print(restaurant_lookup(restaurant_info, 'moderate', 'centre', 'italian'))
+if __name__ == "__main__":
+    main()
